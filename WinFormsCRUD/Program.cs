8d14b35019ec6611aea90e4953023e19469a350c
JR-CIAC <jreydc.ciac@gmail.com>
2026-06-25 11:26:18 +0800

    Uploaded the WindowsForm Template

---
+++ WinFormsCRUD/Program.cs
@@ -0,0 +1,30 @@
+using System.Configuration;
+using WinFormsCRUD.Script.Component.Profile;
+using WinFormsCRUD.Script.Globals;
+using WinFormsCRUD.Script.Manager;
+
+namespace WinFormsCRUD;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        ConnectionConfig.ConnectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+        var _mainForm = new Form1();
+        var _eventSubscriptionManager = new EventSubscriptionManager();
+        var _profileRepository = new ProfileRepository();
+        var _profileService = new ProfileService(_profileRepository);
+        var _profileController = new ProfileController(_mainForm, _profileService, _eventSubscriptionManager);
+
+        
+        Application.Run(_mainForm);
+    }
+}
